83109917864c2e37f4e8a3f9a17af7ef23e3d16d
bannawandoor27 <bannawandoor@gmail.com>
2024-11-19 16:25:42 +0530

    fix: update setup py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='cadmium-django-sdk',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         'requests>=2.0.0',
         'Django>=3.0',
     ],
     description='Cadmium SDK for capturing and sending Django errors.',
-    author='Your Name',
-    author_email='your_email@example.com',
-    url='https://github.com/yourusername/cadmium-django-sdk',
+    author='bannawandoor',
+    author_email='connect@hasanulbanna.in',
+    url='https://github.com/bannawandoor27/cadmium-django-sdk',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Framework :: Django',
